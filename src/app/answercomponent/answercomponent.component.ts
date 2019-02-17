f9992976f77471e742fe6e1129b4684c54ef1ede
nomanwaris <nomanwaris0347@gmail.com>
2019-02-17 19:43:33 +0500

    answers posted

--- src/app/answercomponent/answercomponent.component.ts
+++ src/app/answercomponent/answercomponent.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-answercomponent',
@@ -12,18 +13,36 @@ export class AnswercomponentComponent implements OnInit {
 
   constructor(public route: ActivatedRoute, private http: HttpClient) { }
   questionID: string;
-  answers ;
+  answers = [];
   ngOnInit() {
+    this.fetchAnswer();
+  }
+  onPostAnswer(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log('answer');
+    this.http.post('http://localhost:3000/postanswer', {
+      'answer':  form.value.answer,
+      'questionID': this.answers['_id']})
+      .subscribe((data) => {
+      console.log(data);
+      this.fetchAnswer();
+    });
+
+
+  }
+  fetchAnswer() {
     this.route.params.subscribe( (params) => {
       console.log(params.id);
       this.http.post('http://localhost:3000/answers', {'questionID': params.id}).subscribe((data) => {
         console.log(data[0]['answer']);
         this.answers = data[0];
+        console.log(this.answers);
 
 
       });
     } );
-
   }
 
 
