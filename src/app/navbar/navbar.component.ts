3011b8cfa7a695eb419b80f85b09760f9ae652c0
unknown <saqibzuhair@gmail.com>
2019-02-24 14:57:21 +0500

    navbar updated

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgForm} from '@angular/forms';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   title: 'Ask and Freelance';
+  user;
+  loggedIn = false;
+  current = '/main/freelance';
 
-  constructor() { }
+  constructor(private router: Router, private location: Location) {
+  }
 
   ngOnInit() {
+    if (localStorage.getItem('userid') != null) {
+      this.loggedIn = true;
+      this.user = {
+        'userid': localStorage.getItem('userid'),
+        'username': localStorage.getItem('username')
+      };
+    }
+
+  }
+
+  signout() {
+    this.loggedIn = false;
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
+
+  onSearch(form: NgForm) {
+
+    console.log(form.value);
+      if ((location.pathname).toString().substring(0, this.current.length) === this.current) {
+      console.log(location.pathname);
+      // freelance component
+    } else {
+      console.log(location.pathname + '  else');
+      // question component
+    }
+
   }
 
 }
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {Location} from '@angular/common';
+
+import {QuestionService} from '../services/question.service';
+import {until} from 'selenium-webdriver';
+import elementIsSelected = until.elementIsSelected;
 
 
 @Component({
@@ -12,13 +17,20 @@ export class QuestionsComponent implements OnInit {
 
   Question;
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, private question: QuestionService, private location: Location) {
+  }
 
   ngOnInit() {
-    this.http.post('http://localhost:3000/questions', {}).subscribe((data) => {
-      console.log(data);
-      this.Question = data;
-    });
+    if (location.pathname === '/main') {
+      localStorage.setItem('component', 'q/a');
+      this.question.getQuestions(undefined);
+      this.question.questionObservable.subscribe(value => {
+        this.Question = value;
+      });
+    } else {
+      console.log(location.pathname);
+      // yeh user k apnay question hn
+    }
 
   }
 
@@ -26,4 +38,5 @@ export class QuestionsComponent implements OnInit {
     console.log('asdasd');
     this.router.navigate(['main/postquestion']);
   }
+
 }
